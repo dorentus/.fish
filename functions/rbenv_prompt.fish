ca151334985547072bbfe9407529f4234adb3fd9
ZHANG Yi <zhangyi.cn@gmail.com>
2014-07-17 23:39:31 +0800

    test if rbenv exists

--- functions/rbenv_prompt.fish
+++ functions/rbenv_prompt.fish
@@ -1,9 +1,12 @@
 function rbenv_prompt
-  set -l rb_version (rbenv local 2> /dev/null)
-  if [ "$rb_version" != "" ]
-    set_color 62A; printf "rb=%s🍄 " $rb_version
+  if which rbenv > /dev/null
+    set -l rb_version (rbenv local 2> /dev/null)
+    if [ "$rb_version" != "" ]
+      set_color 62A; printf "rb=%s🍄 " $rb_version
+    else
+      printf "$argv"
+    end
   else
     printf "$argv"
   end
-
 end
