935d129cd605c4184f97b50e346a3fb43cc9466f
Zhang Yi <zhangyi.cn@gmail.com>
2014-07-17 22:02:13 +0800

    fix tabs

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,20 +1,20 @@
 function fish_prompt
   set -l last_status $status
 
-	if not set -q __fish_prompt_computername
-		set -g __fish_prompt_computername (scutil --get ComputerName)
-	end
+  if not set -q __fish_prompt_computername
+    set -g __fish_prompt_computername (scutil --get ComputerName)
+  end
 
-	if not set -q __fish_prompt_symbol
-		set -g __fish_prompt_symbol (~/bin/choose_by_hostname 🍏 🍡 👾 ⭕ 🌀 🌐)
-	end
+  if not set -q __fish_prompt_symbol
+    set -g __fish_prompt_symbol (~/bin/choose_by_hostname 🍏 🍡 👾 ⭕ 🌀 🌐)
+  end
 
   if not test $last_status -eq 0
     set_color $fish_color_error
   else
-	  set_color black
+    set_color black
   end
-  printf "\n┌─"	
+  printf "\n┌─"  
 
   set_color black
   printf $USER
@@ -26,11 +26,11 @@ function fish_prompt
   set_color black
   printf $__fish_prompt_computername
 
-	set_color normal
-	printf ":"
+  set_color normal
+  printf ":"
 
-	set_color blue
-	printf '%s' (echo $PWD | sed -e "s|^$HOME|~|")
+  set_color blue
+  printf '%s' (echo $PWD | sed -e "s|^$HOME|~|")
 
   if not test $last_status -eq 0
     set_color $fish_color_error
@@ -40,10 +40,10 @@ function fish_prompt
   printf "\n└───"
 
   set_color black
-	printf "%s " $__fish_prompt_symbol
+  printf "%s " $__fish_prompt_symbol
 
-	set_color normal
-	printf "%s " (__fish_git_prompt)
+  set_color normal
+  printf "%s " (__fish_git_prompt)
 
   set_color normal
 end
