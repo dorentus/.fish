9248e8fa94d255d0bc0f6863f455b53a24b479fd
Zhang Yi <zhangyi.cn@gmail.com>
2014-07-18 00:06:26 +0800

    disable fish_right_prompt on Linux

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,3 +1,13 @@
 function fish_right_prompt
-  rbenv_prompt (set_color black; today_prompt '🐠 ')
+  if not set -q __fish_right_prompt_emoji_bugged
+    if test (uname) = "Linux"
+      set -g __fish_right_prompt_emoji_bugged yes
+    else
+      set -g __fish_right_prompt_emoji_bugged no
+    end
+  end
+
+  if test $__fish_right_prompt_emoji_bugged = "no"
+    rbenv_prompt (set_color black; today_prompt '🐠 ')
+  end
 end
