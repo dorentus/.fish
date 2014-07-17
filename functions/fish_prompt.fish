0924cf42ccf3b39a2329d8fc9c11d25d207c8f66
ZHANG Yi <zhangyi.cn@gmail.com>
2014-07-17 23:40:16 +0800

    fix behavior, to be the same as original choose_by_hostname.rb

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -13,7 +13,7 @@ function fish_prompt
     begin
       set -l symbols 🍏 🍡 👾 ⭕ 🌀 🌐
       set -l symbols_count (count $symbols)
-      set -l hosthash (hostname | tr -d '\n' | shasum)
+      set -l hosthash (hostname -f | tr -d '\n' | shasum)
       set -l selected_index (ruby -e "print '$hosthash'.to_i(16) % $symbols_count + 1")
       set -g __fish_prompt_symbol $symbols[$selected_index]
     end
